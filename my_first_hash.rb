bc603737ea34af57700805a66bdcc2e83f338433
Christina Delavar <c.delavar@gmail.com>
2018-09-15 19:02:11 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -17,7 +17,6 @@ def retrieval
                  }
 
   shipping_manifest["oil paintings"]
-
 end
 
 def adding
@@ -26,14 +25,11 @@ def adding
                   "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
-  # add 2 muskets to the shipping_manifest hash below
+  shipping_manifest["muskets"] = 2
+
+  shipping_manifest["gun powder"] = 4
 
 
-
-  # add 4 gun powder to the shipping_manifest hash below
-
-
-
-  # return the shipping_manifest hash below
+  shipping_manifest
 
 end
